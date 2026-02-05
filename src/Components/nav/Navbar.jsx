0e34a72533001e5dd1d0ba10a959c2cd36aed5e3
codeArt001 <olaniyanoluwajomiloju25@gmail.com>
2026-02-05 14:55:36 +0100

    initial commit

--- src/Components/nav/Navbar.jsx
+++ src/Components/nav/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { HiMenuAlt3, HiX } from "react-icons/hi";
-import ThemeToggle from "../nav/ThemeToggle";
+import ThemeToggle from "./ThemeToggle.jsx";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
