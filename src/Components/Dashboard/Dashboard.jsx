5a02f40cc29f9e0f2ae6719aee07fb8596bb9ae3
codeArt001 <olaniyanoluwajomiloju25@gmail.com>
2026-02-05 15:55:48 +0100

    initial commit

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -2,7 +2,6 @@ import Activities from "./DashboardActivity/Activities";
 import Devicetable from "./Devicetable";
 import OverviewCard from "./DashboardActivity/OverviewCard";
 import DashboardBar from "./DashboardBar";
-// import DashboardBar from "./DashboardBar";
 
 const Dashboard = () => {
   return (
